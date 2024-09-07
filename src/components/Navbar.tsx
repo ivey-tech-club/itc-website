3d37faf693fe7680de5e867cb4cf3d4e4b5d3b7e
elyknehc <kchen727@uwo.ca>
2024-09-06 20:04:59 -0400

    add smooth scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,25 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import itc from "../assets/itc.png";
+
 const Navbar = () => {
+	const handleSmoothScroll = (
+		e: React.MouseEvent<HTMLAnchorElement>,
+		targetId: string
+	) => {
+		e.preventDefault(); // Prevent default anchor behavior
+		const targetElement = document.querySelector(targetId); // Find the target section by ID
+
+		// Scroll to the target section smoothly
+		if (targetElement) {
+			targetElement.scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			});
+		}
+	};
+
 	return (
 		<nav className="bg-[#E9F2EB] border-b border-gray-200 font-syne ">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -14,30 +32,34 @@ const Navbar = () => {
 					</div>
 					<div className="flex-shrink-0 flex items-center">
 						{/* Navigation Links */}
-						<Link
+						<a
 							href="#about"
-							className="text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							className="nav-link text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							onClick={(e) => handleSmoothScroll(e, "#about")}
 						>
 							About
-						</Link>
-						<Link
+						</a>
+						<a
 							href="#events"
-							className="text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							className="nav-link text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							onClick={(e) => handleSmoothScroll(e, "#events")}
 						>
 							Events
-						</Link>
-						<Link
+						</a>
+						<a
 							href="#team"
-							className="text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							className="nav-link text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							onClick={(e) => handleSmoothScroll(e, "#team")}
 						>
 							Team
-						</Link>
-						<Link
+						</a>
+						<a
 							href="#contact"
-							className="text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							className="nav-link text-iveyGreen hover:text-gray-900 font-semibold mr-5"
+							onClick={(e) => handleSmoothScroll(e, "#contact")}
 						>
 							Contact
-						</Link>
+						</a>
 					</div>
 				</div>
 			</div>
